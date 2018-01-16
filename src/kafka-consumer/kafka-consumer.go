776425fc8a2196f1475c5a15d2007784282c0c06
Tarun Vashisth <tarun.vashisth@target.com>
2018-01-16 23:37:57 +0530

    Add comments on producer and consumer snippets

--- src/kafka-consumer/kafka-consumer.go
+++ src/kafka-consumer/kafka-consumer.go
@@ -7,17 +7,21 @@ import (
 )
 
 func main() {
+
+	//Get a consumer; nil config is provided
 	consumer, err := kafka.NewConsumer([]string{"localhost:9092"}, nil)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Connection Successful !!")
 
+	//Consumer messages from single partition
 	messages, err := consumer.ConsumePartition("test-topic", 0, kafka.OffsetNewest)
 	if err != nil {
 		panic(err)
 	}
 
+	//Acknowledge messages and errors
 	fmt.Println("Printing consumed messages....")
 	for i := 0; i < 10; i++ {
 		select {
@@ -28,5 +32,6 @@ func main() {
 		}
 	}
 
+	//Close consumer to avoid leaks
 	consumer.Close()
 }
--- src/kafka-producer/kafka-producer.go
+++ src/kafka-producer/kafka-producer.go
@@ -8,15 +8,19 @@ import (
 )
 
 func main() {
+	//Setup config
 	config := kafka.NewConfig()
 	config.Producer.Flush.Messages = 10
 	config.Producer.Return.Successes = true
+
+	//Get a producer object
 	producer, err := kafka.NewAsyncProducer([]string{"localhost:9092"}, config)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("Connection Successful !!")
 
+	//Send producer messages to Input channel
 	for i := 0; i < 10; i++ {
 		producer.Input() <- &kafka.ProducerMessage{
 			Topic: "test-topic",
@@ -25,6 +29,7 @@ func main() {
 		}
 	}
 
+	//Catch errors and success acknowledgement
 	for i := 0; i < 10; i++ {
 		select {
 		case msg := <-producer.Errors():
@@ -34,5 +39,6 @@ func main() {
 		}
 	}
 
+	//Close producer to avoid leaks
 	producer.Close()
 }
